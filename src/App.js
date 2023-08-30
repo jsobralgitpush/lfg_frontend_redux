ea44e3714b5840ea564c449f40635167ef8858a9
ze <jose.sobral@poli.ufrj.br>
2023-08-30 16:23:02 -0300

    feat(add proposal): Add ProposalForm, List and Tast to our app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
+import ProposalList from './features/proposal/ProposalList';
+import ProposalForm from './features/proposal/ProposalForm';
+import { ToastAlert } from './utils/features/alert/Toast';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App max-h-[100vh]">
+      <div className='w-full p-5'>
+        <h1 className='text-3xl font-bold'>Loans For Good</h1>
+      </div>
+      <div className='flex px-10'>
+        <div className="w-1/2 p-5 max-h-[100vh] overflow-y-auto">
+          <h1 className="text-xl py-2">Request your proposal with us right now!</h1>
+          <ProposalForm />
+        </div>
+        <div className="w-1/2 p-5 max-h-[100vh] overflow-y-auto">
+          <h1 className="text-xl py-2">Check out our recent Proposals!</h1>
+          <ProposalList />
+        </div>
+      </div>
+      <div className="fixed left-2 bottom-2">
+        <ToastAlert />
+      </div>
     </div>
   );
 }
