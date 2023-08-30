70ccfe7c1066f5f2681991a9dc7af733f6bdc4aa
ze <jose.sobral@poli.ufrj.br>
2023-08-30 16:25:58 -0300

    feat(add proposal): Add apiConstants file

---
+++ src/constants/apiConstants.js
@@ -0,0 +1,6 @@
+const API_HOSTNAME = 'http://localhost:8000';
+
+
+export const FETCH_PROPOSALS_URL = `${API_HOSTNAME}/api/proposal/`;
+export const FETCH_PROPOSAL_ATTRIBUTES_URL = `${API_HOSTNAME}/api/proposal/attributes`;
+export const SEND_PROPOSAL_URL = `${API_HOSTNAME}/api/proposal/`;
