3ddbec3a009ae10e5cf321e9eeae7382bd8f5d08
ze <jose.sobral@poli.ufrj.br>
2023-08-30 16:25:20 -0300

    feat(add proposal): Add postProposal to our app

---
+++ src/api/proposal/postProposal.js
@@ -0,0 +1,21 @@
+import axios from 'axios';
+import store from '../../app/store';
+import { 
+  sendProposalSuccess,
+  sendProposalFailure
+} from '../../features/proposal/proposalSlice';
+import { SEND_PROPOSAL_URL } from '../../constants/apiConstants';
+import { CSRF_TOKEN } from '../../utils/methods/getCsrfToken';
+
+export const postProposal = async (formData) => {
+  try {
+    const response = await axios.post(SEND_PROPOSAL_URL, formData, {
+      headers: {
+        'X-CSRFToken': CSRF_TOKEN
+      }
+    })
+    store.dispatch(sendProposalSuccess(response.data))
+  } catch(err) {
+    store.dispatch(sendProposalFailure())
+  }
+}
