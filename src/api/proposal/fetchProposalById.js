83a8e1e47c6d4c9c11fa0cf92ed0733e56725ce5
ze <jose.sobral@poli.ufrj.br>
2023-08-30 16:25:08 -0300

    feat(add proposal): Add fetchProposalById to our app

---
+++ src/api/proposal/fetchProposalById.js
@@ -0,0 +1,16 @@
+import axios from 'axios';
+import store from '../../app/store';
+import { 
+  refreshProposalSuccess,
+  refreshProposalFailure
+} from '../../features/proposal/proposalSlice';
+import { FETCH_PROPOSALS_URL } from '../../constants/apiConstants';
+
+export const fetchProposalById = async (proposalId) => {
+  try {
+    const response = await axios.get(`${FETCH_PROPOSALS_URL}${proposalId}`);
+    store.dispatch(refreshProposalSuccess(response.data))
+  } catch(err) {
+    store.dispatch(refreshProposalFailure())
+  }
+}
