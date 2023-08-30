b716ec8fe7d738a916b029f1643b3060f3e9c822
ze <jose.sobral@poli.ufrj.br>
2023-08-30 16:24:21 -0300

    feat(add proposal): Add fetchProposal file to fetch our proposals

---
+++ src/api/proposal/fetchProposal.js
@@ -0,0 +1,19 @@
+import axios from 'axios';
+import store from '../../app/store';
+import { 
+  getProposals,
+  getProposalsSuccess,
+  getProposalsFailure,
+} from '../../features/proposal/proposalSlice';
+import { FETCH_PROPOSALS_URL } from '../../constants/apiConstants';
+
+export const fetchProposal = async () => {
+  store.dispatch(getProposals())
+
+  try {
+    const response = await axios.get(FETCH_PROPOSALS_URL)
+    store.dispatch(getProposalsSuccess(response.data))
+  } catch (err) {
+    store.dispatch(getProposalsFailure())
+  }
+}
