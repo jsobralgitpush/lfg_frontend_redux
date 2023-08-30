cbbe1b3996025c0a367380a22e25ee027e528a90
ze <jose.sobral@poli.ufrj.br>
2023-08-30 16:26:15 -0300

    feat(add proposal): Add ProposalForm component

---
+++ src/features/proposal/ProposalForm.js
@@ -0,0 +1,123 @@
+import React, { useEffect, useState } from 'react';
+import { connect, useSelector } from 'react-redux';
+import { 
+  sendProposalSuccess,
+  sendProposalFailure,
+  getProposalAttributes,
+  getProposalAttributesSuccess,
+  getProposalAttributesFailure,
+ } from './proposalSlice';
+import { fetchProposalAttributes } from '../../api/proposal/fetchProposalAttributes';
+import { postProposal } from '../../api/proposal/postProposal';
+import { Label, TextInput, Button } from 'flowbite-react';
+
+
+const ProposalForm = () => {
+  const attributes = useSelector((state) => state.proposal.attributes);
+  const [formData, setFormData] = useState({});
+
+  const capitalizeFirstLetter = (string) => {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  }
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setFormData(prevState => ({
+      ...prevState,
+      [name]: value
+    }));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(formData)
+    postProposal(formData)
+  }
+
+  useEffect(() => {
+    fetchProposalAttributes()
+  }, []);
+
+  return (
+    <form
+      className="flex flex-col gap-4"
+      onSubmit={handleSubmit}
+    >
+      <div>
+        <div className='flex'>
+          <div className='w-1/2 p-2'>
+            <Label
+              htmlFor='name'
+              value='Name'
+            />
+            <TextInput
+              name='name'
+              id='name'
+              placeholder='Enter your full name here'
+              required
+              value={formData.name || ''}
+              onChange={handleInputChange}
+            />
+          </div>
+          <div className='w-1/2 p-2'>
+            <Label
+              htmlFor='document'
+              value='ID Number'
+            />
+            <TextInput
+              name='document'
+              id='document'
+              placeholder='Enter your ID number here'
+              required
+              type='number'
+              value={formData.document || ''}
+              onChange={handleInputChange}
+            />
+          </div>
+        </div>
+        <hr className='p-2 mt-2'></hr>
+        {attributes.map((attribute) => (
+          <div key={attribute.name} className='py-1'>
+            <div className='mb-2 block text-left'>
+              <Label
+                htmlFor={attribute.name}
+                value={capitalizeFirstLetter(attribute.name)}
+              />
+            </div>
+            <TextInput
+              name={attribute.slug}
+              id={attribute.slug}
+              placeholder={`Enter your ${attribute.name} here`}
+              required
+              value={formData[attribute.name] || ''}
+              onChange={handleInputChange}
+            />
+
+          </div>
+        ))}
+      </div>
+      <Button
+        type='submit'
+        color='green'
+        className='w-1/2 m-auto'
+      >
+        Submit
+      </Button>
+    </form>
+  )
+}
+
+const mapStateProps = (store) => ({
+  attributes: store.proposal.attributes,
+})
+
+const mapDispatchToProps = (dispatch) => ({
+  getProposalAttributes,
+  getProposalAttributesSuccess,
+  getProposalAttributesFailure,
+  sendProposalSuccess,
+  sendProposalFailure,
+})
+
+
+export default connect(mapStateProps, mapDispatchToProps)(ProposalForm);
